c1dbdf72980d1199674e2d5458b7192c5d48d5c4
Nimish Vohra <136614864+NimishVohra07@users.noreply.github.com>
2023-07-07 20:05:02 +0530

    Add files via upload

---
+++ The Cake Catalogue (Project 2)/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import TheCakeFairy from "/Users/nimishvohra/Desktop/The Cake Catalogue (Project 2)/src/components/Basic/TheCakeFairy.js"
+
+function App() {
+  return <TheCakeFairy />
+}
+
+export default App
---
+++ The Cake Catalogue (Project 2)/src/components/Basic/menuApi.js
@@ -0,0 +1,230 @@
+const Menu = [
+    {
+      id: 1,
+      image: "Images/blackforest.jpeg",
+      name: "Black Forest Cake",
+      category: "Cakes",
+      price: "₹ 1200/kg",
+      description:
+        " This deliciously moist Black Forest Cake is a cut above the rest with homemade whipped cream, rich chocolate ganache, and sweet spiked cherries. ",
+    },
+  
+    {
+      id: 2,
+      image: "Images/vanilla-cake-4.webp",
+      name: "Vanilla cake",
+      category: "Cakes",
+      price: "₹ 1000/kg",
+      description:
+        "With its outstanding vanilla flavor, pillowy soft crumb, and creamy vanilla buttercream, this is truly the best vanilla cake. ",
+    },
+    {
+      id: 3,
+      image: "Images/Butterscotch-1727x1577.jpg.webp",
+      name: "Butterscotch Cake",
+      category: "Cakes",
+      price: "₹ 1100/kg",
+      description:
+        "This butterscotch cake has tender moist brown butter cake layers, a silky smooth butterscotch buttercream. ",
+    },
+    {
+      id: 4,
+      image: "Images/download.jpeg",
+      name: "Pineapple Cake",
+      category: "Cakes",
+      price: "₹ 900/kg",
+      description:
+        " It is moist and fluffy, with a mild pineapple flavor and crushed pineapple baked right in.",
+    },
+    {
+      id: 5,
+      image: "Images/sq-choco-truffle-cake0005choc-AA.avif",
+      name: "Chocolate Cake",
+      category: "Cakes",
+      price: "₹ 1500/kg",
+      description:
+        "a chocolate layer cake recipe with dense, moist chocolate cake, silky chocolate truffle frosting and chocolate flakes. ",
+    },
+    {
+      id: 6,
+      image: "Images/3d-Customized-Cake.jpg",
+      name: "Customized Cakes",
+      category: "Cakes",
+      price: "₹ 2000/kg",
+      description:
+        " With custom cakes, you don't just get something delicious; you also get something memorable. ",
+    },
+    {
+      id: 7,
+      image: "Images/brookies.jpeg",
+      name: " Brookies",
+      category: "Cookies and Biscuits",
+      price: "₹ 800/kg",
+      description:
+        "These Brookies are rich, fudgy and decadent bars made with half brownie and half chocolate chip cookie.",
+    },
+    {
+      id: 8,
+      image: "Images/homemade-dark-chocolate-bar-500x500.webp",
+      name: " Dark Chocolates",
+      category: "Chocolates",
+      price: "₹ 400/kg",
+      description:"",
+    },
+    {
+      id: 9,
+      image: "Images/mendiants-500x500.jpg",
+      name: "White Chocolates",
+      category: "Chocolates",
+      price: "₹ 400/kg",
+      description:
+        "",
+    },
+    {
+      id: 10,
+      image: "Images/dry-fruit-chocolate.png",
+      name: "Dry fruit Chocolates",
+      category: "Chocolates",
+      price: "₹ 500/kg",
+      description:"",
+    },
+    {
+      id: 11,
+      image: "Images/paan.jpeg",
+      name: "Paan Chocolates",
+      category: "Chocolates",
+      price: "₹ 500/kg",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 12,
+      image: "Images/mango.jpeg",
+      name: "Mango flavoured Chocolates",
+      category: "Chocolates",
+      price: "₹ 500/kg",
+      description:
+        ""
+    },
+    {
+      id: 13,
+      image: "Images/hamper.jpeg",
+      name: " Chocolate hamper",
+      category: "Gift Hampers",
+      price: "₹ 800",
+      description:
+        "A box containing assorted chocolates",
+    },
+    
+    {
+      id: 14,
+      image: "Images/download (1).jpeg",
+      name: "Naankhatai",
+      category: "Cookies and Biscuits",
+      price: "₹ 600/kg",
+      description:
+        " Naankhatais of different flavours are comprised in the box",
+    },
+    {
+      id: 15,
+      image: "Images/1_COMPRESSED.png",
+      name: "Assorted Cookies",
+      category: "Gift Hampers",
+      price: "₹ 700/kg",
+      description:
+        " A box of 1kg includes different types of cookies",
+    },
+    {
+      id: 16,
+      image: "Images/dry fruit.jpeg",
+      name: "Dry fruit Biscuits",
+      category: "Cookies and Biscuits",
+      price: "₹ 800/kg",
+      description:
+        "A handmade delight with the goodness of crushed almonds, cashews and pistachios." 
+    },
+    {
+      id: 17,
+      image: "Images/Brownies.jpeg",
+      name: "Brownies",
+      category: "Cakes",
+      price: "₹ 1000",
+      description:
+        " A pack contains 10 pieces of brownies. ",
+    },
+    {
+      id: 18,
+      image: "Images/cake jar.jpeg",
+      name: "Cake jars",
+      category: "Gift Hampers",
+      price: "₹ 600",
+      description:
+        " A pack contains 6 cake jars of different flavours ",
+    },
+    {
+      id: 19,
+      image: "Images/AttaBiscuit-1_1200x1200.webp",
+      name: "Atta Biscuits",
+      category: "Cookies and Biscuits",
+      price: "60₹",
+      description:
+        "ATTA BISCUITS (COOKIES) are best as they are made up of Whole Wheat Atta that contains higher quantity of Dietary Fiber. ",
+    },
+    {
+      id: 20,
+      image: "Images/cupcakes.jpeg",
+      name: "Cupcakes",
+      category: "Gift Hampers",
+      price: "₹ 900",
+      description:
+        "A box contains 12 cupcakes of different flavours. ",
+    },
+    {
+      id: 21,
+      image: "Images/butter.jpeg",
+      name: "Butter Cookies",
+      category: "Cookies and Biscuits",
+      price: "₹ 700/kg",
+      description:
+        "These baked goodies are categorized in The Biscuit Collection as crisp cookies because of their texture.",
+    },
+    {
+      id: 22,
+      image: "Images/blueberry.jpeg",
+      name: "Blueberry Cheesecake",
+      category: "Cakes",
+      price: "₹ 1800/kg",
+      description:
+        " This light and creamy cheesecake sits on top of a biscuit layer and is topped by layer of blueberries.",
+    },
+    {
+      id: 23,
+      image: "Images/pastries.jpeg",
+      name: "Pack of 10 pastries",
+      category: "Gift Hampers",
+      price: "₹1000",
+      description:
+        "A box containing 10 pastries with different flavours.",
+    },
+    {
+      id: 24,
+      image: "Images/chistrmas cake.jpeg",
+      name: " Christmas cake",
+      category: "Cakes",
+      price: "₹1200/kg",
+      description:
+        "Christmas Cake is a traditional fruit cake with a rich, velvety texture that's so full flavoured and moist it can be eaten plain.  ",
+    },
+    {
+      id: 25,
+      image: "Images/maxresdefault.jpg",
+      name: " Tea cakes",
+      category: "Cakes",
+      price: "₹400/kg",
+      description:
+        " In most of England, a teacake is a light, sweet, yeast-based bun containing dried fruits, most usually currants, sultanas or peel. ",
+    },
+
+  ];
+  
+  export default Menu;
